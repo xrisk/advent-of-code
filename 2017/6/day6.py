1b30c1a8dea85a1140e103e8bd8d76eae40f58b2
Rishav <rishav.kundu98@gmail.com>
2017-12-06 15:20:58 +0530

    Add solutions for day 6

---
+++ 2017/6/day6.py
@@ -0,0 +1,34 @@
+blocks = [int(x) for x in input().split()]
+
+look = set([])
+look.add(tuple(blocks))
+
+cnt = 0
+while True:
+
+    max_val = -1111111
+    max_pos = -1
+
+    for i, j in enumerate(blocks):
+        if j > max_val:
+            max_val = j
+            max_pos = i
+
+    blocks[max_pos] = 0
+
+    while max_val != 0:
+        max_pos = (max_pos + 1) % len(blocks)
+        blocks[max_pos] += 1
+        max_val -=1
+
+    cnt += 1
+
+    if tuple(blocks) in look:
+        print(cnt)
+        break
+    else:
+        look.add(tuple(blocks))
+
+
+
+
