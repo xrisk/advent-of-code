55ba1c946b220c8a3bb6aef29147bef3a93ba4f3
Rishav <rishav.kundu98@gmail.com>
2017-12-11 18:37:14 +0530

    Add day 11

---
+++ 2017/11/day11.py
@@ -0,0 +1,28 @@
+line = input().split(',')
+
+x = 0
+y = 0
+
+def dist(x, y):
+    x, y = abs(x), abs(y)
+    return int(x / 2 + y)
+
+def move(d):
+    global x, y
+    if d == 'n':
+        y += 1
+    elif d == 'ne':
+        x, y = x + 1, y + 1/2
+    elif d == 'se':
+        x, y = x + 1, y - 1/2
+    elif d == 's':
+        y -= 1
+    elif d == 'sw':
+        x, y = x - 1, y - 1/2
+    elif d == 'nw':
+        x, y = x - 1, y + 1/2
+    return dist(x, y)
+
+
+print([move(d) for d in line][-1])
+
